a4782f8cfd2550f84c65ae554922cb59686d02b6
Eduarda <eduardaoliveiragomess@gmail.com>
2021-05-07 09:50:25 -0300

    alteração na entidade endereço, foi adicionado métodos get

--- entidade/endereco.py
+++ entidade/endereco.py
@@ -5,5 +5,24 @@ class Endereco:
         self.__numero = numero
         self.__complemento = complemento
 
+    @property
+    def bairro(self):
+        retunr self.__bairro
+    
+    @property
+    def rua(self):
+        retunr self.__rua
+
+    @property
+    def numero (self):
+        retunr self.__numero
+
+    @property
+    def complemento(self):
+        retunr self.__complemento
+    
+
+
+
     def mostrar_endereco(self):
         return f"{self.__bairro}, {self.__rua}, {self.__numero}, {self.__complemento}"
