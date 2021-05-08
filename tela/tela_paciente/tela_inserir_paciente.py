2d9e54aed32affa66149853341122313f8c14fbe
Eduarda <eduardaoliveiragomess@gmail.com>
2021-05-08 18:51:32 -0300

    alteração do layout da tela inserir paciente

--- tela/tela_paciente/tela_inserir_paciente.py
+++ tela/tela_paciente/tela_inserir_paciente.py
@@ -12,18 +12,18 @@ class TelaInserirPaciente():
         sg.ChangeLookAndFeel('Reddit') 
     
         layout = [
-            [sg.Text('Preencha os campos a seguir')],  
-            [sg.Text('Nome', size=(30, 1)), sg.InputText(nome,  key='nome')], 
-            [sg.Text('Telefone', size=(30, 1)), sg.InputText(telefone,  key='telefone')], 
-            [sg.Text('CPF', size=(30, 1)), sg.InputText(cpf,  key='cpf')],
-            [sg.Text('Bairro', size=(30, 1)), sg.InputText(bairro,  key='bairro')],
-            [sg.Text('Rua', size=(30, 1)), sg.InputText(rua,  key='rua')],
-            [sg.Text('Número', size=(30, 1)), sg.InputText(numero,  key='numero')], 
-            [sg.Text('Complemento', size=(30, 1)), sg.InputText(complemento,  key='complemento')],
-            [sg.Text('Data de nascimento(DD/MM/AAAA)', size=(30, 1)), sg.InputText(data_nascimento,  key='data_nascimento')], 
-            [sg.Button('Salvar'), sg.Button('Sair')]
+            [sg.Text('Cadastro Paciente', size=(20, 1),text_color='#4682B4', font=("Helvetica", 16, 'bold'))],  
+            [sg.Text('Nome', size=(30, 1), font=("Helvetica", 15)), sg.InputText(nome,  key='nome', font=("Helvetica", 15))], 
+            [sg.Text('Telefone', size=(30, 1), font=("Helvetica", 15)), sg.InputText(telefone,  key='telefone', font=("Helvetica", 15))], 
+            [sg.Text('CPF', size=(30, 1), font=("Helvetica", 15)), sg.InputText(cpf,  key='cpf', font=("Helvetica", 15))],
+            [sg.Text('Bairro', size=(30, 1), font=("Helvetica", 15)), sg.InputText(bairro,  key='bairro', font=("Helvetica", 15))],
+            [sg.Text('Rua', size=(30, 1), font=("Helvetica", 15)), sg.InputText(rua,  key='rua', font=("Helvetica", 15))],
+            [sg.Text('Número', size=(30, 1),font=("Helvetica", 15)), sg.InputText(numero,  key='numero', font=("Helvetica", 15))], 
+            [sg.Text('Complemento',size=(30, 1), font=("Helvetica", 15)), sg.InputText(complemento,  key='complemento', font=("Helvetica", 15))],
+            [sg.Text('Data de nascimento(DD/MM/AAAA)', size=(30, 1),font=("Helvetica", 15)), sg.InputText(data_nascimento,  key='data_nascimento', font=("Helvetica", 15))], 
+            [sg.Button('Salvar', font=("Helvetica", 15),size=(5,1)), sg.Button('Sair', font=("Helvetica", 15),size=(5,1))]
         ] 
-        self.__window = sg.Window('Cadastramento').Layout(layout) 
+        self.__window = sg.Window('Cadastramento', size=(800,330)).layout(layout)
     
 
     def open(self):
